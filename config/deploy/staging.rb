5c8b86366f112c89ce2d0686205a64f92eb48c33
Florian Rieder <florian.rieder@unil.ch>
2025-06-24 14:21:40 +0200

    run ci again

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :deploy_to, '/srv/staging-api.gos.ch'
 # set a branch for this release
 set :branch, 'dev'
 
-# test: define environment-specific server
+# define environment-specific server
 server 'tstsgg.unil.ch', port: '44144', user: 'tstsgg', roles: %w{app db web}
 
 # Map docker and docker-compose commands
